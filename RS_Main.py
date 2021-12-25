08c309e224b3c7cc8a8194521c0ae8ea0dafdac1
robert-kuo <kaku@datawise.com.tw>
2021-12-25 22:29:52 +0800

    Create RS_Main.py

---
+++ RS_Main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+myapp = Flask(__name__)
+
+
+@myapp.route("/")
+def hello():
+    return 'RS Service ........'
+
+@myapp.route("/test")
+def test():
+    return 'robert tttt !!  ...'
