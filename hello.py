77541ac7914b444561fc673e769c716af455906f
robert-kuo <kaku@datawise.com.tw>
2021-12-25 21:29:50 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,3 +6,7 @@ myapp = Flask(__name__)
 @myapp.route("/")
 def hello():
     return 'Hello World! ...   ??? !!!!!!!!!!!!!!!!!!!'
+
+@myapp.route("/test")
+def test():
+    return 'robert tttt !!  ...'
